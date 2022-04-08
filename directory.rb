c4e2b140c0e4196d412a9b2fd4688703aded12f2
Anish <anishkakaiya@outlook.com>
2022-04-08 17:58:56 +0100

    add filesave.csv function

--- directory.rb
+++ directory.rb
@@ -13,6 +13,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -23,6 +24,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -68,6 +71,18 @@ end
 #prints footer from show_students function
 def print_footer
   puts "Overall, we have #{@students.count} great students"
+end
+
+#saves to .csv file
+def save_students
+  file = File.open("students.csv", "w")
+
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
