f1240dc7370b2bf4c800f55eae1c58ffe4a313b8
Anish <anishkakaiya@outlook.com>
2022-04-08 15:49:08 +0100

    refactor code

--- directory.rb
+++ directory.rb
@@ -1,58 +1,73 @@
-# First, we print the list of students
+#instance variable to be accessed by any method
+@students = []
+
+#interactive menu to select from process function
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+#prints the menu from interactive_menu
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+#takes user input from interactive_menu and makes a choice
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again."
+  end
+end
+
+#enter students into hash within @students list.
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  
-  students = []
 
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
 
     name = gets.chomp
   end
-  students
 end
+
+#prints header, list of students and footer
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+#prints header from show_students function
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(student)
-  student.each do |student|
+#prints list of students from show_students function
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
-
-def interactive_menu
-  students = []
-  loop do
-  # 1. print the menu and ask the user what to do
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
-  # 2. read the input and save it into a variable
-  input = gets.chomp
-  # 3. do what the user asked
-  case input
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again."
-    end
-  end
+#prints footer from show_students function
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
