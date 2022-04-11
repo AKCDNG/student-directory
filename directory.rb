19c0ce87c30bd5a01d9c7b14c0cd998906993f78
Anish <anishkakaiya@outlook.com>
2022-04-11 14:48:00 +0100

    add function to load files if they exist from command line

--- directory.rb
+++ directory.rb
@@ -5,7 +5,7 @@
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -41,13 +41,13 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  name = gets.chomp
+  name = STDIN.gets.chomp
 
   while !name.empty? do
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
 
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -97,6 +97,17 @@ def load_students (filename="students.csv")
   file.close
 end
 
+def try_loading_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exist?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
 
-
+try_loading_students
 interactive_menu
